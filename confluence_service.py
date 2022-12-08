bd4a91ea8e7129d4b0fb876e7b990594a15d745a
Azimnoryushan <azimnoryushan@gmail.com>
2022-12-08 15:23:46 +0800

    Added sub space section

--- confluence_service.py
+++ confluence_service.py
@@ -32,47 +32,87 @@ def create_dashboard(space = None, dashboard_name = None):
     return status
 
 # TODO: Clean up messy logic :(
-# /update/DSDEA/Access+EzBuild/Access+EzBuild+-+Analytics/dashboard_summary
-@app.route('/update/<space>/<project>/<parent>/<dashboard_name>')
-def update_dashboard(space = None, project = None, parent = None, dashboard_name = None):
+# /update/DevServices/Advanced+Add-On+Packs/Access+EzBuild/Access+EzBuild+-+Analytics/dashboard_summary
+@app.route('/update/<space>/<sub_space>/<project>/<parent>/<dashboard_name>')
+def update_dashboard(space = None, sub_space = None, project = None, parent = None, dashboard_name = None):
     # Update an existing dashboard
     try:
         # Clean up URL
+        sub_space = sub_space.replace('+', ' ')
         project = project.replace('+', ' ')
         parent = parent.replace('+', ' ')
+
+        #return space + "/" + sub_space + "/" + project + "/" + parent + "/" + dashboard_name
         dashboard_name = dashboard_name.replace('+', '_')
 
         dashboard_info = requests.get(os.getenv('DASHBOARD_URL') + dashboard_name).json()
         title = dashboard_info['Dashboard Name']
         body = render_template('page_template', dashboard_info=dashboard_info)
+        
+        #Check if sub_space page exists
+        if confluence.page_exists(space=space, title=sub_space):
+            sub_space_id = confluence.get_page_id(
+                space=space, 
+                title=sub_space
+            )
 
-        # Check project page exists
-        if confluence.page_exists(space=space, title=project):
-            project_id = confluence.get_page_id(space=space, title=project)
+            # Check project page exists
+            if confluence.page_exists(space=space, title=project):
+                project_id = confluence.get_page_id(
+                    space=space, 
+                    title=project
+                )
 
-            # Check parent page exists
-            if confluence.page_exists(space=space, title=parent):
-                parent_id = confluence.get_page_id(space=space, title=parent)
-
-                # Check dashboard page exists
-                if confluence.page_exists(space=space, title=title):
-                    page_id = confluence.get_page_id(
+                # Check parent page exists
+                if confluence.page_exists(space=space, title=parent):
+                    parent_id = confluence.get_page_id(
                         space=space, 
-                        title=title
+                        title=parent
                     )
-                    confluence.update_page(page_id=page_id, parent_id=parent_id, title=title, body=body)
-                    return "Successfully updated dashboard: " + dashboard_name
+
+                    # Check dashboard page exists
+                    if confluence.page_exists(space=space, title=title):
+                        page_id = confluence.get_page_id(
+                            space=space, 
+                            title=title
+                        )
+                        confluence.update_page(page_id=page_id, sub_space_id=sub_space_id, parent_id=parent_id, title=title, body=body)
+                        return "Successfully updated dashboard: " + dashboard_name
+                    else:
+                        confluence.create_page(space=space, parent_id=parent_id, title=title, body=body)
+                        return "Successfully created dashboard: " + dashboard_name
+
                 else:
+                    create_child_page(space=space, parent_id=project_id, title=parent)
+                    parent_id = confluence.get_page_id(space=space, title=parent)
                     confluence.create_page(space=space, parent_id=parent_id, title=title, body=body)
                     return "Successfully created dashboard: " + dashboard_name
 
             else:
-                create_child_page(space=space, parent_id=project_id, title=parent)
-                parent_id = confluence.get_page_id(space=space, title=parent)
-                confluence.create_page(space=space, parent_id=parent_id, title=title, body=body)
-                return "Successfully created dashboard: " + dashboard_name
+                # Create project page
+                create_parent_page(space=space, title=project)
 
+                project_id = confluence.get_page_id(
+                    space=space, 
+                    title=project
+                )
+                # Create parent page
+                create_child_page(space=space, parent_id=project_id, title=parent)
+
+                parent_id = confluence.get_page_id(
+                    space=space, 
+                    title=parent
+                )
+                # Create dashboard page
+                create_child_page(space=space, parent_id=parent_id, title=title, body=body)
         else:
+            # Create sub_space page
+            create_parent_page(space=space, title=sub_space)
+
+            sub_space_id = confluence.get_page_id(
+                space=space, 
+                title=sub_space
+            )
             # Create project page
             create_parent_page(space=space, title=project)
 
@@ -90,7 +130,7 @@ def update_dashboard(space = None, project = None, parent = None, dashboard_name
             # Create dashboard page
             create_child_page(space=space, parent_id=parent_id, title=title, body=body)
 
-            return "Successfully created dashboard: " + dashboard_name
+        return "Successfully created dashboard: " + dashboard_name
 
     except requests.exceptions.JSONDecodeError:
         return "No Dashboard API for " + dashboard_name
